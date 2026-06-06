e3671d75e40dfbcf5685025f4b32a6bc45dd8080
Ally Piechowski <alex@piechowski.io>
2026-06-06 22:07:25 +0700

    Bump version to 0.2.3

--- velocity/src/main/kotlin/io/github/grepsedawk/civdiscord/velocity/CivDiscordVelocityPlugin.kt
+++ velocity/src/main/kotlin/io/github/grepsedawk/civdiscord/velocity/CivDiscordVelocityPlugin.kt
@@ -80,7 +80,7 @@ import java.util.concurrent.TimeUnit
 @Plugin(
     id = "civdiscord",
     name = "CivDiscord",
-    version = "0.2.2",
+    version = "0.2.3",
     description = "Discord <-> Minecraft bridge bot (Velocity side)",
     authors = ["grepsedawk"],
 )
